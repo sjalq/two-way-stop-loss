1a713611a597992c0cea83bb82c17a4ac92411ad
Schalk Dormehl <schalk.dormehl@gmail.com>
2022-09-17 16:33:14 +0200

    Implementing ElmUI

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -1,6 +1,17 @@
 module Frontend exposing (Model, app)
 
-import Html exposing (Html, text)
+
+import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
+import Html.Attributes
+
+
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 import Lamdera exposing (sendToBackend)
@@ -8,7 +19,6 @@ import Types exposing (..)
 import Html.Events exposing (onInput)
 import Html.Attributes exposing (type_)
 import Types exposing (FrontendMsg(..))
-import Helpers
 import Helpers exposing (..)
 import Decimal exposing (Decimal)
 import Http
@@ -170,56 +180,100 @@ updateFromBackend msg model =
 view : Model -> Html FrontendMsg
 view model =
     let
-        labeledInput label msg =
-            Html.div []
-                [ Html.label [] [ Html.text label ]
-                , Html.input [ onInput msg ] []
-                ]
+        a = 1
+        -- labeledInput label msg =
+        --     Html.div []
+        --         [ Html.label [] [ Html.text label ]
+        --         , Html.input [ onInput msg ] []
+        --         ]
 
-        labeledValue label value =
-            Html.div []
-                [ Html.label [] [ Html.text label ]
-                , Html.div [] [ Html.text value ]
-                ]
+        -- labeledValue label value =
+        --     Html.div []
+        --         [ Html.label [] [ Html.text label ]
+        --         , Html.div [] [ Html.text value ]
+        --         ]
 
-        labeledButton label msg =
-            Html.button [ onClick msg ] [ Html.text label ]
+        -- labeledButton label msg =
+        --     Html.button [ onClick msg ] [ Html.text label ]
             
         apiView = 
-            Html.div 
+            column 
                 []     
-                [ Html.text "API Connection"
-                , labeledInput "Key" KeyChanged
-                , labeledInput "Secret" SecretChanged
-                , labeledValue "Key" model.apiConnection.key
-                , labeledValue "Secret" model.apiConnection.secret
-                , labeledButton "Change"  ChangeApiConnection
+                [ text "API Connection"
+                , Input.text 
+                    [] 
+                    { onChange = KeyChanged
+                    , text = model.apiConnection.key
+                    , placeholder =
+                        Just <|
+                            Input.placeholder [] <|
+                                text "Enter your API key"
+                    , label = Input.labelAbove [] <| text "API Key"
+                    }
+                , Input.text 
+                    [] 
+                    { onChange = KeyChanged
+                    , text = model.apiConnection.secret
+                    , placeholder =
+                        Just <|
+                            Input.placeholder [] <|
+                                text "Enter your API key"
+                    , label = Input.labelAbove [] <| text "API Key"
+                    }
+                , Element.text model.apiConnection.key
+                , Element.text model.apiConnection.secret
+                , Input.button buttonStyle { onPress = Just ChangeApiConnection, label = text "Update API Connection" }
                 ]
 
-        positionConfigView = 
-            Html.div
-                []
-                [ Html.text "Position Config"
-                , labeledInput "Asset" AssetChanged
-                , labeledInput "Denominating Asset" DenominatingAssetChanged 
-                , labeledInput "Down Trigger Price" TriggerPriceChanged |> Html.map DownStopOrderChange
-                , labeledInput "Down Stop Price" LimitPriceChanged |> Html.map DownStopOrderChange
-                , labeledInput "Up Trigger Price" TriggerPriceChanged |> Html.map UpStopOrderChange
-                , labeledInput "Up Stop Price" LimitPriceChanged |> Html.map UpStopOrderChange
-                , labeledValue "Asset" (model.positionConfig |> mapStringOrBlank .asset)
-                , labeledValue "Denominating Asset" (model.positionConfig |> mapStringOrBlank .denominatingAsset)
-                , labeledValue "Down Trigger Price" (model.positionConfig |> mapDecimalOrBlank (.downStop >> .triggerPrice))
-                , labeledValue "Down Stop Price" (model.positionConfig |> mapDecimalOrBlank (.downStop >> .limitPrice))
-                , labeledValue "Up Trigger Price" (model.positionConfig |> mapDecimalOrBlank (.upStop >> .triggerPrice))
-                , labeledValue "Up Stop Price" (model.positionConfig |> mapDecimalOrBlank (.upStop >> .limitPrice))
-                , labeledButton "Update Position Config" ChangePositionConfig
-                ]
-    in
-        Html.div [ style "padding" "30px" ]
-            [ Html.button [ onClick (CounterChange Increment) ] [ text "+" ]
-            , Html.text (String.fromInt model.counter)
-            , Html.button [ onClick (CounterChange Decrement) ] [ text "-" ]
-            , Html.div [] [ Html.text "Click me then refresh me!" ]
-            , apiView |> Html.map ApiConnectionChange
-            , positionConfigView |> Html.map PositionConfigChange
+        -- positionConfigView = 
+        --     Html.div
+        --         []
+        --         [ Html.text "Position Config"
+        --         , labeledInput "Asset" AssetChanged
+        --         , labeledInput "Denominating Asset" DenominatingAssetChanged 
+        --         , labeledInput "Down Trigger Price" TriggerPriceChanged |> Html.map DownStopOrderChange
+        --         , labeledInput "Down Stop Price" LimitPriceChanged |> Html.map DownStopOrderChange
+        --         , labeledInput "Up Trigger Price" TriggerPriceChanged |> Html.map UpStopOrderChange
+        --         , labeledInput "Up Stop Price" LimitPriceChanged |> Html.map UpStopOrderChange
+        --         , labeledValue "Asset" (model.positionConfig |> mapStringOrBlank .asset)
+        --         , labeledValue "Denominating Asset" (model.positionConfig |> mapStringOrBlank .denominatingAsset)
+        --         , labeledValue "Down Trigger Price" (model.positionConfig |> mapDecimalOrBlank (.downStop >> .triggerPrice))
+        --         , labeledValue "Down Stop Price" (model.positionConfig |> mapDecimalOrBlank (.downStop >> .limitPrice))
+        --         , labeledValue "Up Trigger Price" (model.positionConfig |> mapDecimalOrBlank (.upStop >> .triggerPrice))
+        --         , labeledValue "Up Stop Price" (model.positionConfig |> mapDecimalOrBlank (.upStop >> .limitPrice))
+        --         , labeledButton "Update Position Config" ChangePositionConfig
+        --         ]
+        buttonStyle = 
+            [ padding 5
+            -- , alignLeft
+            , Border.width 2
+            , Border.rounded 6
+            , Border.color color.blue
+            , Background.color color.lightBlue
             ]
+    in
+        layout [ padding 10 ] <|
+            row [ spacing 10 ]
+                [ Input.button 
+                    buttonStyle { onPress = Just (CounterChange Increment), label = text "+" }
+                , text (String.fromInt model.counter)
+                , Input.button buttonStyle { onPress = Just (CounterChange Decrement), label = text "-" }
+                , apiView |> Element.map ApiConnectionChange
+                ]
+
+        -- Html.div [ style "padding" "30px" ]
+        --     [ Html.button [ onClick (CounterChange Increment) ] [ text "+" ]
+        --     , Html.text (String.fromInt model.counter)
+        --     , Html.button [ onClick (CounterChange Decrement) ] [ text "-" ]
+        --     , Html.div [] [ Html.text "Click me then refresh me!" ]
+        --     , apiView |> Html.map ApiConnectionChange
+        --     , positionConfigView |> Html.map PositionConfigChange
+        --     ]
+
+color =
+    { blue = rgb255 0x72 0x9F 0xCF
+    , darkCharcoal = rgb255 0x2E 0x34 0x36
+    , lightBlue = rgb255 0xC5 0xE8 0xF7
+    , lightGrey = rgb255 0xE0 0xE0 0xE0
+    , white = rgb255 0xFF 0xFF 0xFF
+    }
